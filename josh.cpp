1f7518136af32fb58a4ec8084f5b7bd6c1bf9632
joshuaA100 <42805595+joshuaA100@users.noreply.github.com>
2018-09-12 22:03:43 +0000

    Add files via upload

---
+++ josh.cpp
@@ -0,0 +1,24 @@
+
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter first number: ";
+	cin>>a;
+	cout<<"Enter second number: ";
+	cin>>n;
+	int ab=(a,b;)
+	int GCD=1;
+	for(int i=1;i<=ab;i++)
+	{
+		if(a%i==0 && a%i==0)
+		{
+			GCD=max(GCD,i);
+		}
+	}
+	cout<<"Greatest common divis is: "<<GCD;
+	
+	return 0;
+}
